16114ba74cf7322b3f5ac2d6726cdc8873619a30
D S <daniel.siegle@gmail.com>
2023-06-02 11:28:09 -0400

    too hot! lowered temperature

--- litchat.py
+++ litchat.py
@@ -6,7 +6,7 @@ st.title('Parrot Chain Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=2.0, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=0.4, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
 with st.form('my_form'):
