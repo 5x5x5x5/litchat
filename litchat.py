378e05d0ca37aff69ed8e4cc2e929ea5d56b06b7
Daniel Siegle <daniel.siegle@gmail.com>
2023-07-11 18:35:13 -0400

    rollback changes

--- litchat.py
+++ litchat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('Leprechaun Quickstart App')
+st.title('Personality Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
